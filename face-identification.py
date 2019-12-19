8efda6cb993742db1d601f041c5cccd72e6f15cd
Mrigank Mehta <mrigank.mehta@mail.utoronto.ca>
2019-12-18 20:51:58 -0500

    Implemented face detection.

--- face-identification.py
+++ face-identification.py
@@ -1,17 +1,24 @@
 import numpy as np
 import cv2
 
+face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_alt2.xml')
 cap = cv2.VideoCapture(0)
 
 while(True):
-    # Capture frame-by-frame
     ret, frame = cap.read()
-
-    # Display the resulting frame
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        img_item = "me.png"
+        cv2.imwrite(img_item, roi_gray)
+        blue = (255, 0, 0)
+        stroke = 2
+        cv2.rectangle(frame, (x, y), (x + w, y + h), blue, stroke)
     cv2.imshow('frame', frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
