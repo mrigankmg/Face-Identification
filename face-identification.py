23699d0c036344372f7aa7ad93a791addc790f66
Mrigank Mehta <mrigank.mehta@mail.utoronto.ca>
2019-12-19 14:54:39 -0500

    Changed detection from using haar classifier to dnn caffemodel.

--- face-identification.py
+++ face-identification.py
@@ -1,22 +1,35 @@
 import numpy as np
+import os
+import imutils
 import cv2
 
-face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_alt2.xml')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_dir = os.path.join(BASE_DIR, "model")
+
+caffeModel = os.path.join(model_dir, 'res10_300x300_ssd_iter_140000.caffemodel')
+prototextPath = os.path.join(model_dir, 'deploy.prototxt.txt')
+
+net = cv2.dnn.readNetFromCaffe(prototextPath, caffeModel)
 cap = cv2.VideoCapture(0)
 
-while(True):
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-    for (x, y, w, h) in faces:
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
-        img_item = "me.png"
-        cv2.imwrite(img_item, roi_gray)
-        blue = (255, 0, 0)
-        stroke = 2
-        cv2.rectangle(frame, (x, y), (x + w, y + h), blue, stroke)
-    cv2.imshow('frame', frame)
+while True:
+    _, frame = cap.read()
+    frame = imutils.resize(frame, width=700)
+    h, w = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+                                 (300, 300), (104.0, 177.0, 123.0))
+    net.setInput(blob)
+    detections = net.forward()
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence >= 0.5:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            startX, startY, endX, endY = box.astype("int")
+            text = "{:.2f}%".format(confidence * 100)
+            y = startY - 10 if startY - 10 > 10 else startY + 10
+            cv2.rectangle(frame, (startX, startY), (endX, endY), (255, 0, 0), 2)
+            cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+    cv2.imshow("Frame", frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
