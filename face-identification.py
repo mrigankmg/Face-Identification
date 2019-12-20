a13a4b9ffc6ad075f93e1df3778f6a5efd6a1d65
Mrigank Mehta <mrigank.mehta@mail.utoronto.ca>
2019-12-20 02:12:58 -0500

    Fixed small bug.

--- face-identification.py
+++ face-identification.py
@@ -2,10 +2,9 @@ import numpy as np
 import os
 import imutils
 import cv2
-from PIL import Image
+import pickle
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-image_dir = os.path.join(BASE_DIR, "imgs")
 model_dir = os.path.join(BASE_DIR, 'model')
 
 caffeModel = os.path.join(model_dir, 'res10_300x300_ssd_iter_140000.caffemodel')
@@ -14,39 +13,13 @@ prototextPath = os.path.join(model_dir, 'deploy.prototxt.txt')
 net = cv2.dnn.readNetFromCaffe(prototextPath, caffeModel)
 cap = cv2.VideoCapture(0)
 
-curr_id = 0
-labelToId = {}
 idToLabel = {}
-train_x = []
-train_y = []
+with open("labels.pickle", "rb") as f:
+    labelToId = pickle.load(f)
+    idToLabel = {v: k for k, v in labelToId.items()}
+
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-
-for root, dirs, files in os.walk(image_dir):
-    for file in files:
-        if file.endswith("png") or file.endswith("jpg") or file.endswith("jpeg"):
-            path = os.path.join(root, file)
-            label = os.path.basename(root).lower()
-            idToLabel[curr_id] = label
-            if labelToId.get(label) is None:
-                labelToId[label] = curr_id
-                curr_id += 1
-            image = cv2.imread(path)
-            h, w = image.shape[:2]
-            blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
-            net.setInput(blob)
-            detections = net.forward()
-            confidence = detections[0, 0, 0, 2]
-            if confidence >= 0.5:
-                box = detections[0, 0, 0, 3:7] * np.array([w, h, w, h])
-                startX, startY, endX, endY = box.astype("int")
-                pil_image = Image.open(path).convert("L")
-                # final_image = pil_image.resize((150, 150), Image.LANCZOS)
-                image_array = np.array(pil_image, "uint8")
-                roi = image_array[startY:endY, startX:endX]
-                train_x.append(roi)
-                train_y.append(labelToId[label])
-
-recognizer.train(train_x, np.array(train_y))
+recognizer.read("model.yml")
 
 while True:
     _, frame = cap.read()
@@ -69,7 +42,7 @@ while True:
             cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 1)
             if pred_confidence >= 0.5:
                 y = startY - 10 if startY - 10 > 10 else startY + 10
-                cv2.putText(frame, pred_label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+                cv2.putText(frame, pred_label, (startX, y), cv2.FONT_HERSHEY_DUPLEX, 0.6, (0, 255, 0), 2)
     cv2.imshow("Face Identification", frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
---
+++ train.py
@@ -0,0 +1,56 @@
+import numpy as np
+import os
+import cv2
+import pickle
+from PIL import Image
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+image_dir = os.path.join(BASE_DIR, "imgs")
+model_dir = os.path.join(BASE_DIR, 'model')
+
+caffeModel = os.path.join(model_dir, 'res10_300x300_ssd_iter_140000.caffemodel')
+prototextPath = os.path.join(model_dir, 'deploy.prototxt.txt')
+
+net = cv2.dnn.readNetFromCaffe(prototextPath, caffeModel)
+
+curr_id = 0
+labelToId = {}
+train_x = []
+train_y = []
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+for root, dirs, files in os.walk(image_dir):
+    for file in files:
+        if file.endswith("png") or file.endswith("jpg") or file.endswith("jpeg"):
+            path = os.path.join(root, file)
+            label = os.path.basename(root).lower()
+            if labelToId.get(label) is None:
+                labelToId[label] = curr_id
+                curr_id += 1
+            image = cv2.imread(path)
+            h, w = image.shape[:2]
+            blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+            net.setInput(blob)
+            detections = net.forward()
+            confidence = detections[0, 0, 0, 2]
+            if confidence >= 0.5:
+                box = detections[0, 0, 0, 3:7] * np.array([w, h, w, h])
+                startX, startY, endX, endY = box.astype("int")
+                pil_image = Image.open(path).convert("L")
+                image_array = np.array(pil_image, "uint8")
+                roi_gray = image_array[startY:endY, startX:endX]
+                cv2.imwrite("curr.jpg", roi_gray)
+                pil_image = Image.open("curr.jpg").convert("L")
+                final_image = pil_image.resize((350, 350), Image.ANTIALIAS)
+                image_array = np.array(final_image, "uint8")
+                train_x.append(image_array)
+                train_y.append(labelToId[label])
+
+os.remove("curr.jpg")
+
+with open("labels.pickle", "wb") as f:
+    pickle.dump(labelToId, f)
+
+recognizer.train(train_x, np.array(train_y))
+recognizer.save("model.yml")
